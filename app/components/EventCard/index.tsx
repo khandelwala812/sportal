5f08c93a24a0073098869d193c2aac8b4eb7d914
Avi <43710887+khandelwala812@users.noreply.github.com>
2022-03-21 12:39:20 -0700

    task: editing event

--- app/components/EventCard/index.tsx
+++ app/components/EventCard/index.tsx
@@ -3,7 +3,11 @@ import React, { FC } from "react"
 import * as SC from "./styles"
 import { IEvent } from "../../types"
 
-const EventCard: FC<IEvent> = ({ name, startTime, endTime }) => {
+interface IEventCardProps extends IEvent {
+  setEditing: () => void
+}
+
+const EventCard: FC<IEventCardProps> = ({ name, startTime, endTime, setEditing }) => {
   const formatTime = () => {
     if (sameMeridiem(startTime, endTime)) {
       return `${startTime}-${endTime + startTime.meridiem}`
@@ -12,11 +16,17 @@ const EventCard: FC<IEvent> = ({ name, startTime, endTime }) => {
     return `${startTime + startTime.meridiem} - ${endTime + endTime.meridiem}`
   }
   
+  const handlePress = () => {
+    setEditing(true)
+  }
+  
   return (
     <SC.CardWrapper>
       <SC.Title>{name}</SC.Title>
       <SC.Subtitle>{formatTime()}</SC.Subtitle>
-      <SC.EditButton name="pencil" size={24} color="black" />
+      <SC.EditButton onPress={handlePress}>
+        <SC.Pencil name="pencil" size={24} color="black" />
+      </SC.EditButton>
     </SC.CardWrapper>
   )
 }
