299d06c2466c9ae20776405ed8ca1ffb8700cd59
Avi <43710887+khandelwala812@users.noreply.github.com>
2022-03-21 09:52:44 -0700

    chore: add time to event card

--- app/components/EventCard/index.tsx
+++ app/components/EventCard/index.tsx
@@ -3,10 +3,19 @@ import React, { FC } from "react"
 import * as SC from "./styles"
 import { IEvent } from "../../types"
 
-const EventCard: FC<IEvent> = ({ name }) => {
+const EventCard: FC<IEvent> = ({ name, startTime, endTime }) => {
+  const formatTime = () => {
+    if (sameMeridiem(startTime, endTime)) {
+      return `${startTime}-${endTime + startTime.meridiem}`
+    }
+    
+    return `${startTime + startTime.meridiem}-${endTime + endTime.meridiem}`
+  }
+  
   return (
     <SC.CardWrapper>
       <SC.Title>{name}</SC.Title>
+      <SC.Subtitle>{formatTime()}</SC.Subtitle>
     </SC.CardWrapper>
   )
 }
