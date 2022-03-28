8edc55a04a6c50bb0ea97a841009a8a151f0b409
Avi <43710887+khandelwala812@users.noreply.github.com>
2022-03-28 11:01:12 -0700

    ui: event card

--- app/pages/events/styles.tsx
+++ app/pages/events/styles.tsx
@@ -1,6 +1,19 @@
 import styled from "styled-components"
 import Button from "../../components/Button"
 
+export const EventWrapper = styled.View`
+  height: 50px;
+  justify-content: center;
+`
+
+export const Image = styled.Image`
+  width: 20%;
+`
+
+export const DetailsWrapper = styled.View`
+  flex: 1;
+`
+
 export const RegisterButton = styled(Button)`
   width: 30%;
 `
