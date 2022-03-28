2ad008c528783f1516e57da7d8501be52c9c73e4
Avi <43710887+khandelwala812@users.noreply.github.com>
2022-03-28 13:58:18 -0700

    task: add registerForEvent

--- app/api/users.ts
+++ app/api/users.ts
@@ -3,10 +3,17 @@ import client from "./client"
 
 const route = "/users"
 
-const getEvents = (userId?: string) => {
+const getEvents = (userId: string) => {
   return client.get<IUserEvent[]>(`${route}/events/${userId}`)
 }
 
+const registerForEvent = (userId: string, eventId: string) => {
+  return client.post(`${route}/register-event`, {
+    data: { userId, eventId }
+  })
+}
+
 export default {
-  getEvents
+  getEvents,
+  registerForEvent
 }
