a4359b8bd54bd585b3043d91fb5c98db931cb18c
Avi <43710887+khandelwala812@users.noreply.github.com>
2022-04-05 14:10:02 -0700

    task: formatDate function

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,1 +1,9 @@
 export const joinWithCommas = (words: string[]) => words.join(", ")
+
+export const formatDate = (date: Date) => {
+  const month = date.getMonth()
+  const day = date.getDate()
+  const year = date.getFullYear()
+  
+  return `${month} ${day}, ${year}`
+}
